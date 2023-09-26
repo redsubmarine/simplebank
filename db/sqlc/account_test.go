4f65ba6055b46f2928b1c3dc106b5ab812e7085b
Wonseok Yang <wsyang@digicaps.com>
2023-09-26 11:25:30 +0900

    add util random for testing

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/redsubmarine/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "tom",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
