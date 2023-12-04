05bf85cbb05ce2afaa4463a6d6ddaad79f14916c
Wonseok Yang <wsyang@digicaps.com>
2023-12-04 16:35:54 +0900

    avoid deadlock

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -61,9 +61,6 @@ var txKey = struct{}{}
 // It creates a transfer record, add account entries, and update accounts' balance within a single database transaction
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (result TransferTxResult, err error) {
 	err = store.execTx(ctx, func(q *Queries) error {
-		txName := ctx.Value(txKey)
-		fmt.Println(txName, "create transfer")
-
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
 			ToAccountID:   arg.ToAccountID,
@@ -73,7 +70,6 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (resul
 			return err
 		}
 
-		fmt.Println(txName, "create entry1")
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.FromAccountID,
 			Amount:    -arg.Amount,
@@ -82,7 +78,6 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (resul
 			return err
 		}
 
-		fmt.Println(txName, "create entry2")
 		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.ToAccountID,
 			Amount:    arg.Amount,
@@ -106,15 +101,16 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (resul
 func addMoney(
 	ctx context.Context,
 	q *Queries,
-	accountID1 int64,
-	amount1 int64,
-	accountID2 int64,
+	accountID1,
+	amount1,
+	accountID2,
 	amount2 int64,
-) (account1 Account, account2 Account, err error) {
+) (account1, account2 Account, err error) {
 	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
 		ID:     accountID1,
 		Amount: amount1,
 	})
+
 	if err != nil {
 		return
 	}
