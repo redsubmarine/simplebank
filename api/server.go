9aac09a542249e183de187ee435820214aa651cd
Wonseok Yang <wsyang@digicaps.com>
2023-12-05 14:17:33 +0900

    add List api

--- api/server.go
+++ api/server.go
@@ -21,6 +21,7 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
 
 	return server
 }
