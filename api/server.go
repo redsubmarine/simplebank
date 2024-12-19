cb2556d84b6af7600998f0706a7b65db3b78b593
Wonseok Yang <wsyang@digicaps.com>
2024-12-19 13:18:18 +0900

    계좌이체 기능 엔드포인트 추가.

--- api/server.go
+++ api/server.go
@@ -26,6 +26,8 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts", server.listAccounts)
 	router.DELETE("/accounts/:id", server.deleteAccount)
 
+	router.POST("/transfers", server.createTransfer)
+
 	return server
 }
 
