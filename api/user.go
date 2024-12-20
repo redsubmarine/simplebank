0f8cbc0f92abb9b016cbad1e4480aa38a8548ac4
red <wonseok@duck.com>
2024-12-20 23:41:32 +0900

    createUser api 추가.

---
+++ api/user.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	db "github.com/redsubmarine/simplebank/db/sqlc"
+	"github.com/redsubmarine/simplebank/util"
+	"net/http"
+)
+
+type CreateUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum,min=3"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req CreateUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pgErr, ok := err.(*pq.Error); ok {
+			switch pgErr.Code.Name() {
+			case "foreign_key_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+}
